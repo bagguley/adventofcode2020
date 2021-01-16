4cf89dcb7dba3d1840b3ad1fedf0d6ce40c6eeb0
Wayne Bagguley <wayne.bagguley@gmail.com>
2021-01-16 12:44:34 +0000

    Day 1 Various one-line solutions

--- src/main/kotlin/day1/part1/Part1.kt
+++ src/main/kotlin/day1/part1/Part1.kt
@@ -3,6 +3,18 @@ package day1.part1
 import day1.data
 
 fun main() {
+    println(sequence {for(a in 0..data.size-2) { for (b in a+1 until data.size) { if (data[a] + data[b] == 2020) yield(data[a] * data[b])} } }.first())
+
+    println((0..data.size-2).asSequence().flatMap{a->(a until data.size).map{ b->data[a] to data[b]}}.find{(x,y)->x+y==2020}?.let{(x,y)->x*y})
+
+    println((0..data.size-2).asSequence().flatMap{a->(a until data.size).map{data[it]}.filter{b->2020-b==data[a]}}.first().let{it*(2020-it)})
+
+    println(data.find{data.minus(it).contains(2020-it)}?.let{(2020-it)*it})
+
+    println(calc())
+}
+
+fun calc() {
     val input = data.toMutableList()
     val result = mutableListOf<Pair<Int,Int>>()
     while (input.size > 1) {
