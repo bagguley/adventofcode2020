577be3d04c96405ea2a943193495661a48261dd2
Wayne Bagguley <wayne.bagguley@gmail.com>
2021-11-29 16:53:27 +0000

    Day 2 oneliners

--- src/main/kotlin/day1/part2/Part2.kt
+++ src/main/kotlin/day1/part2/Part2.kt
@@ -3,6 +3,14 @@ package day1.part2
 import day1.data
 
 fun main() {
+    println(sequence{(0..data.size-3).forEach{a->(1..data.size-2).forEach{b->(2..data.size-1).forEach{c->if(data[a]+data[b]+data[c]==2020) yield(data[a]*data[b]*data[c])}}}}.first())
+
+    println(sequence{data.forEach{a->data.minus(a).forEach{b->data.minus(a).minus(b).forEach{c->if(a+b+c==2020) yield(a*b*c)}}}}.first())
+
+    calc()
+}
+
+fun calc() {
     val input = data.toMutableList()
     val result = mutableListOf<Int>()
     while (input.size > 2) {
